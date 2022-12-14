4eb746369a762dcd4f1295703fda101d9cbc8c07
minari-c <h990727@gmail.com>
2022-12-15 05:38:02 +0900

    fix issue: restart humidity sensor

--- HumiditySensor.py
+++ HumiditySensor.py
@@ -7,12 +7,14 @@ GPIO.setmode(GPIO.BCM)
 
 sensor = Adafruit_DHT.DHT11
 pin = 4
+i = 1
 while True :
-	h, t = Adafruit_DHT.read_retry(sensor, pin, retries=50, delay_seconds=0.1)
+	h, t = Adafruit_DHT.read_retry(sensor, pin, retries=75, delay_seconds=0.001)
 	if h is not None and t is not None :
-		print(f"Temperature = {t:0.1f}*C Humidity = {h:0.1f}%")
+		print(f"[{i}]Temperature = {t:0.1f}*C Humidity = {h:0.1f}%")
 	else:
 		print('Read error')
+	i += 1
 	time.sleep(1)
 
 GPIO.cleanup()

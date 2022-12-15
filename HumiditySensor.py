606bc7b85eeee8fc607412ba91388af27b480ba5
minari-c <h990727@gmail.com>
2022-12-15 09:10:14 +0900

    issue: not work humidity sensor

--- HumiditySensor.py
+++ HumiditySensor.py
@@ -9,12 +9,16 @@ sensor = Adafruit_DHT.DHT11
 pin = 4
 i = 1
 while True :
-	h, t = Adafruit_DHT.read_retry(sensor, pin, retries=75, delay_seconds=0.001)
-	if h is not None and t is not None :
-		print(f"[{i}]Temperature = {t:0.1f}*C Humidity = {h:0.1f}%")
-	else:
-		print('Read error')
-	i += 1
-	time.sleep(1)
-
-GPIO.cleanup()
+	try:
+		time.sleep(1)
+		h, t = Adafruit_DHT.read_retry(sensor, pin, retries=10, delay_seconds=0.008)
+		if h is not None and t is not None :
+			print(f"[{i}]Temperature = {t:0.1f}*C Humidity = {h:0.1f}%")
+		else:
+			print('Read error')
+		i += 1
+		time.sleep(1)
+	except KeyboardInterrupt as e:
+		print(e)
+	finally:
+		GPIO.cleanup()
